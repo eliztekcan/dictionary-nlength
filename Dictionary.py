7a164bd565c98df678a33c84430e850ce8faeea4
eliztekcan <eliztekcan@gmail.com>
2017-04-01 14:50:14 +0300

    Working well

--- Dictionary.py
+++ Dictionary.py
@@ -1,4 +1,5 @@
 from nltk.corpus import wordnet as wn
+from array import array
 
 def count_underscore(word):
     i = 0
@@ -18,4 +19,15 @@ def get_n_length_words( n):
             nLength.add(word.upper())
     return sorted(nLength)
 
+def x( n):
+    nLength = get_n_length_words(n)
+    result = "[\n"
+    i = 0
+    for word in nLength:
+        result = result + "{\n\"word\": \"" + word + "\",\n\"score\": 1" + "\n},\n"
+    result = result[:-2]
+    return result + "\n]"
+
+
 print(get_n_length_words(4))
+print(x( 4))
